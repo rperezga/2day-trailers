40b274949eb419fd44ad8e0918cd11adcb84e069
Roger Perez <rperezga@gmail.com>
2019-07-16 15:40:08 -0400

    After first release to play store

--- android/app/src/main/java/com/daytrailers/MainApplication.java
+++ android/app/src/main/java/com/daytrailers/MainApplication.java
@@ -3,6 +3,10 @@ package com.daytrailers;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airship.customwebview.CustomWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNCWebViewPackage(),
+            new ReactVideoPackage(),
+            new VectorIconsPackage(),
+            new CustomWebViewPackage()
       );
     }
 
